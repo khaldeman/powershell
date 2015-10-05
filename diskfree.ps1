bad399d0adfd45d01be6af37db800e08344d1d64
khaldeman <khaldeman@mhs.org>
2015-10-05 08:48:32 -0500

    Freespace on local drive
    Check WMI for size of a localdisk

---
+++ diskfree.ps1
@@ -0,0 +1,3 @@
+﻿$free = get-WmiObject win32_logicaldisk | Where-Object -Property DeviceID -EQ -Value "C:" | Select-Object -Property FreeSpace
+$free = [math]::Round($free.FreeSpace / 1024 / 1024 / 1024)
+Write-Host  $env:COMPUTERNAME ": " $free "G"
---
+++ filemakerproFavorite.ps1
@@ -0,0 +1,9 @@
+﻿New-Item -Path "HKCU:\Software\FileMaker\FileMaker Pro\10.0\FavHostInfo\Entry0"  -Force 
+Set-ItemProperty -Path "HKCU:\Software\FileMaker\FileMaker Pro\10.0\FavHostInfo\Entry0" -Name "Address" -Value "www.host.com" -Force
+Set-ItemProperty -Path "HKCU:\Software\FileMaker\FileMaker Pro\10.0\FavHostInfo\Entry0" -Name "DisplayName" -Value "Myfavorite Name"  -Force
+Set-ItemProperty -Path "HKCU:\Software\FileMaker\FileMaker Pro\10.0\FavHostInfo\Entry0" -Name "FileOption" -Value "1" -type Dword -Force
+Set-ItemProperty -Path "HKCU:\Software\FileMaker\FileMaker Pro\10.0\FavHostInfo\Entry0" -Name "FileNames" -Value "" -Force
+
+New-Item -Path "HKCU:\Software\FileMaker\FileMaker Pro\10.0\FavHostInfo\Summary" -Force
+Set-ItemProperty -Path "HKCU:\Software\FileMaker\FileMaker Pro\10.0\FavHostInfo\Summary" -Name "Entries" -Value "1" -type Dword  -Force
+
