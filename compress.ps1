9e5a22308f168bf1ccc08f5f3e418020838c2f62
khaldeman <khaldeman@mhs.org>
2015-10-05 08:37:37 -0500

    Powershell to compress a subdirectory recursively

---
+++ compress.ps1
@@ -0,0 +1,22 @@
+﻿function CompressDir($directory)
+{
+    $directory_escaped = $directory.Replace("\", "\\");
+    Write-Host "Compressing: " $directory_escaped
+    $result = (Get-WmiObject -Class win32_directory -ComputerName .   -Filter "Name='$directory_escaped'").CompressEx("",$true)
+  
+}
+
+
+$disk = Get-WmiObject win32_logicaldisk -Filter "deviceID='c:'"
+$freespace = $disk.FreeSpace / 1024 / 1024 / 1024
+Write-Host $env:COMPUTERNAME $freespace 
+if($freespace -lt 20)
+{
+    Write-Host $env:COMPUTERNAME "Compressing" 
+    CompressDir -directory "c:\Skoolbo Common Core"
+    CompressDir -directory "c:\windows\installer"
+
+    $disk = Get-WmiObject win32_logicaldisk -Filter "deviceID='c:'"
+    $freespace = $disk.FreeSpace / 1024 / 1024
+    Write-Host $env:COMPUTERNAME $freespace "M"
+}
